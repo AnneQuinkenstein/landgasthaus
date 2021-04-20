d129f1bf003b66cd27436fcdf36444abf37e7c64
Anne Quinkenstein <a.quinkenstein@gmail.com>
2021-04-20 12:02:28 +0200

    changed inbetween spaces to flexbox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Parallax } from "react-parallax";
-import './App.css'; 
+import "./App.css";
 
 const inlineStyle = {
   // background: '#fff',
@@ -18,11 +18,12 @@ function App() {
         bgImage={process.env.PUBLIC_URL + "/img/pflanzen2.png"}
         strength={-200}
       >
-        <div style={{ height: "100vh", width: "90vw" }}>
-        <div style={inlineStyle}>
+        <div style={{ height: "100vh" }}>
+          <div style={inlineStyle}>
             {" "}
             <h1>Herzlich Willkommen in unserem</h1>
-            <img classname="logo"
+            <img
+              className="logo"
               src={process.env.PUBLIC_URL + "/img/imageedit_9_4031107841.png"}
               alt="logo"
             />
@@ -30,26 +31,53 @@ function App() {
           </div>
         </div>
       </Parallax>
-      <div  className="inbetween">
-        <p>
-          Unser Gasthaus ist idyllisch gelegen im Schieferland zwischen Mayen,
-          dem Tor zur Eifel und Cochem an der Mosel. Unsere Küche bietet Ihnen
-          ein reichhaltiges leckeres Angebot an kleinen Speisen und
-          Hauptgerichten sowie Salatvariationen und Eisdesserts.
-        </p>
-        <div>
-          <img
-            src={process.env.PUBLIC_URL + "/img/FB_IMG_Benediktiner.jpg"}
-            alt="Schild"
-            style={{ height: "180px" }}
-            />
-          />
-          <img
-            src={process.env.PUBLIC_URL + "/img/IMG-20200815-WA0002.jpg"}
-            alt="Schild"
-            style={{ height: "180px" }}
-            />
-          />
+      <div className="inbetween">
+        <div className="box1">
+          <p>
+            Unser Gasthaus ist idyllisch gelegen im Schieferland zwischen Mayen,
+            dem Tor zur Eifel und Cochem an der Mosel.{" "}
+          </p>
+          <p>
+            Unsere Küche bietet Ihnen ein reichhaltiges leckeres Angebot an
+            kleinen Speisen und Hauptgerichten sowie Salatvariationen und
+            Eisdesserts.{" "}
+          </p>
+          <p>
+            {" "}
+            Größere Feierlichkeiten, wie z.B. Geburtstage, sind bis zu 65
+            Personen möglich. Sehr gerne bieten wir in Absprache mit Ihnen auch
+            ein Menüfolge an.
+          </p>
+        </div>
+        <div className="box2">
+          <div>
+            <div className="imgContainer">
+              <img
+                src={process.env.PUBLIC_URL + "/img/FB_IMG_Benediktiner.jpg"}
+                alt="Schild"
+              />
+            </div>
+            <div className="imgContainer">
+              <img
+                src={process.env.PUBLIC_URL + "/img/IMG-20200815-WA0002.jpg"}
+                alt="Schild"
+              />
+            </div>
+          </div>
+          <div>
+            <div className="imgContainer">
+              <img
+                src={process.env.PUBLIC_URL + "/img/FB_IMG_Biergarten2.jpg"}
+                alt="Biergarten"
+              />
+            </div>
+            <div className="imgContainer">
+              <img
+                src={process.env.PUBLIC_URL + "/img/FB_IMG_Schieferland.jpg"}
+                alt="Schild"
+              />
+            </div>
+          </div>
         </div>
       </div>
       <Parallax
@@ -57,15 +85,59 @@ function App() {
         strength={600}
       >
         <div style={{ height: 800 }}>
-          <div style={inlineStyle}>
-          </div>
+          <div style={inlineStyle}></div>
         </div>
       </Parallax>
-      <p>
-        Größere Feierlichkeiten, wie z.B. Geburtstage, sind bis zu 65 Personen
-        möglich. Sehr gerne bieten wir in Absprache mit Ihnen auch ein Menüfolge
-        an.
-      </p>
+      <div className="inbetween">
+        <div className="box1">
+          <p>
+            Genießen Sie bei gutem Wetter Ihren Aufenthalt in unserem
+            gemütlichen Biergarten.
+          </p>
+          <p>
+            {" "}
+            Übernachtungen sind in der angeschlossenen Pension (3 Doppelzimmer)
+            möglich. Zudem gibt es eine separate Ferienwohnung.{" "}
+          </p>
+          <p>
+            Unsere Lage bietet die ideale Basis für einen erholsamen Urlaub oder
+            Wochenende mit schönen Wandermöglichkeiten. Zur Moselregion sind es
+            nur 16 km. Laacher See, Nürburgring, Burg Eltz, Trier, Koblenz und
+            die Maarregion sind schnell mit dem Auto zu erreichen!{" "}
+          </p>
+          <p>Wir freuen uns Sie bei uns begrüßen zu dürfen.</p>
+        </div>
+        <div className="box2">
+          <div>
+            <div className="imgContainer">
+              <img
+                src={process.env.PUBLIC_URL + "/img/20210419_175900.jpg"}
+                alt="Schild"
+              />
+            </div>
+            <div className="imgContainer">
+              <img
+                src={process.env.PUBLIC_URL + "/img/FB_IMG_Biergarten1.jpg"}
+                alt="Schild"
+              />
+            </div>
+          </div>
+          <div>
+            <div className="imgContainer">
+              <img
+                src={process.env.PUBLIC_URL + "/img/20210419_175925.jpg"}
+                alt="Biergarten"
+              />
+            </div>
+            <div className="imgContainer">
+              <img
+                src={process.env.PUBLIC_URL + "/img/FB_IMG_Schieferland.jpg"}
+                alt="Schild"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
       <Parallax
         bgImage={process.env.PUBLIC_URL + "/img/Biergarten3.png"}
         strength={200}
@@ -85,10 +157,24 @@ function App() {
         )}
       >
         <div style={{ height: 500 }}>
-          <div style={inlineStyle}>Genießen Sie bei gutem Wetter Ihren Aufenthalt in unserem gemütlichen Biergarten.</div>
+          <div style={inlineStyle}>
+            Genießen Sie bei gutem Wetter Ihren Aufenthalt in unserem
+            gemütlichen Biergarten.
+          </div>
         </div>
       </Parallax>
-      <div style={{ height: "100vh" }}></div>
+      <div style={{ height: "40vh" }}>Kontakt:
+Gasthaus Reichert
+Hauptstr. 4
+Eulgem
+Tel: 02653 7864
+
+
+Öffnungszeiten der Küche:
+Donnerstag bis Samstag von 17:00 Uhr bis 20:00 Uhr
+Sonntag von 11:30 Uhr bis 14:00 Uhr und von 17:00 Uhr bis 20:00 Uhr
+Das Gasthaus ist jeweils bis 23:00 Uhr geöffnet
+Montag bis Mittwoch Ruhetag</div>
     </div>
   );
 }
